db3d38e08aca8fed76baeafa32e999e10fec2e97
Marco Patiño <markdrake.dev@gmail.com>
2019-04-27 14:13:51 -0500

    Feature: Implmented sidebar drawer and navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
+  <v-app dark>
+   <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title><router-link to="/">Home</router-link></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile >
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title><router-link to="/about">About</router-link></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+  </v-navigation-drawer>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+  <v-toolbar app fixed clipped-left>
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>Vuetify</span>
+      <span class="font-weight-light">MATERIAL DESIGN</span>
+    </v-toolbar-title>
+  </v-toolbar>
+
+  <v-content>
+    <router-view/>
+  </v-content>
+
+  <v-footer app fixed>
+    <span>Code Challenge &copy; 2019</span>
+  </v-footer>
   </v-app>
 </template>
 
@@ -30,10 +52,11 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
-    return {
-      //
-    }
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
   }
 }
 </script>
